4509a888dbe619612f7e02d92677477267ac4d17
Quang-Minh Hoang <qmhoang@users.noreply.github.com>
2012-06-04 14:45:02 -0500

    refactored refid and uid so they now are components instead of inherited from

---
+++ DEngine/Actor/UniqueId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DEngine.Actor {
+    /// <summary>
+    /// Global unique id for every game element in the universe
+    /// </summary>
+    public class UniqueId : IComparable<UniqueId>, IEquatable<UniqueId> {
+        public long Id { get; set; }
+
+        public int CompareTo(UniqueId other) {
+            return Id.CompareTo(other.Id);
+        }
+
+        public bool Equals(UniqueId other) {
+            return Id.Equals(other.Id);
+        }
+    }
+}
