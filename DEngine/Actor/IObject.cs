002d5d5fb8df7b8eda789c851a6ff39060e69429
Quang-Minh Hoang <qmhoang@users.noreply.github.com>
2012-06-12 03:05:41 -0500

    remove around some utilities file, deleted unused classes

--- DEngine/Actor/IObject.cs
+++ DEngine/Actor/IObject.cs
@@ -1,14 +1,14 @@
-﻿using DEngine.Actor.Components.Graphics;
-using DEngine.Core;
-using libtcod;
-
-namespace DEngine.Actor {
-//    /// <summary>
-//    /// Interfaces for all entities that can be drawn in the game
-//    /// </summary>
-//    public interface IObject {        
-//        Point Position { get; set; }
-//        Image Image { get; }
-//        string RefId { get; }
-//    }
-}
+﻿//using DEngine.Actor.Components.Graphics;
+//using DEngine.Core;
+//using libtcod;
+//
+//namespace DEngine.Actor {
+////    /// <summary>
+////    /// Interfaces for all entities that can be drawn in the game
+////    /// </summary>
+////    public interface IObject {        
+////        Point Position { get; set; }
+////        Image Image { get; }
+////        string RefId { get; }
+////    }
+//}
