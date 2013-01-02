39c33ad859d112fc9dd4ea5d1b549b0fb67acf45
Quang-Minh Hoang <qmhoang@users.noreply.github.com>
2013-01-02 05:36:05 -0600

    * start of breaking up stuff for componentizing actors, items, and features

--- DEngine/Actor/IObject.cs
+++ DEngine/Actor/IObject.cs
@@ -1,15 +1,37 @@
-﻿//using DEngine.Actor.Components.Graphics;
-//using DEngine.Core;
-//using libtcod;
-//
-//namespace DEngine.Actor {
-////    /// <summary>
-////    /// Interfaces for all entities that can be drawn in the game
-////    /// </summary>
-////    public interface IObject {        
-////        Point Position { get; set; }
-////        Image Image { get; }
-////        string RefId { get; }
-////    }
-//}
+﻿namespace DEngine.Actor {
+	/// <summary>
+	/// Interface for all objects can that do something (ie update)
+	/// </summary>
+	public interface IObject {
+		/// <summary>
+		/// How much action points an entity has
+		/// </summary>
+		int ActionPoints { get; set; }
 
+		/// <summary>
+		/// The rate in which an entity's AP is changed.
+		/// </summary>
+		/// <returns></returns>
+		int Speed { get; }
+
+		/// <summary>
+		/// We have enough AP, execute the action and substract the AP cost for the action
+		/// </summary>
+		void Update();
+
+		/// <summary>
+		/// Is entity dead?
+		/// </summary>
+		bool Dead { get; }
+
+		/// <summary>
+		/// Do something when entity dies
+		/// </summary>
+		void OnDeath();
+
+		/// <summary>
+		/// Can entity call update right now?
+		/// </summary>
+		bool Updateable { get; }
+	}
+}
