02e8b87ac14c45db6c76a570641abcad7d290148
Quang-Minh Hoang <qmhoang@users.noreply.github.com>
2013-01-07 17:08:14 -0600

    * fixed location == * changed map get entities function to include type filtering

--- DEngine/Core/Map.cs
+++ DEngine/Core/Map.cs
@@ -91,8 +91,10 @@ namespace DEngine.Core {
 			ShadowCastingFOV.ComputeRecursiveShadowcasting(FOVMap, viewPoint.X, viewPoint.Y, viewableDistance, true);
 		}
 
-		public IEnumerable<Entity.Entity> GetEntitiesAt(Point location) {
-			return EntityManager.Get<Location>().Where(e => e.Get<Location>().Position == location);
+		public IEnumerable<Entity.Entity> GetEntitiesAt(Point location, params Type[] types) {
+			var l = types.ToList();
+			l.Add(typeof(Location));
+			return EntityManager.Get(l.ToArray()).Where(e => e.Get<Location>().Position == location);
 		}
 	}
 }
