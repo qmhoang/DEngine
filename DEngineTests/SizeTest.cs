90ab82c9994bc9f46efad40360068fbe737c0fbd
Quang-Minh Hoang <Quang-Minh Hoang@localhost>
2013-03-18 20:22:11 -0500

    (IsAdjacentTo) for Point (Area) for Size more tests

---
+++ DEngineTests/SizeTest.cs
@@ -0,0 +1,16 @@
+using DEngine.Core;
+using NUnit.Framework;
+
+namespace DEngineTests {
+	[TestFixture]
+	public class SizeTest {
+		[Test]
+		public void TestArea() {
+			Assert.AreEqual(0, new Size(0, 0).Area);
+			Assert.AreEqual(0, new Size(1, 0).Area);
+			Assert.AreEqual(-1, new Size(1, -1).Area);
+			Assert.AreEqual(4, new Size(-2, -2).Area);
+			Assert.AreEqual(6, new Size(2, 3).Area);
+		} 
+	}
+}
