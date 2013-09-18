ac7cfaa9af0d961b06fab7dac4f1ff87f532cc0e
Quang-Minh Hoang <delirium.q@gmail.com>
2013-09-18 02:41:59 -0500

    forgot some stuff

---
+++ DEngineTests/Core/APTests.cs
@@ -0,0 +1,17 @@
+﻿using DEngine.Actor;
+using NUnit.Framework;
+using System;
+
+namespace DEngineTests.Core
+{
+	[TestFixture]
+	public class APTests
+	{
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void TestNegativeAP()
+		{
+			var ap = new AP(-1);
+		}
+	}
+}
